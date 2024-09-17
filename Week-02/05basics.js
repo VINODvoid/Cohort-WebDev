78136e7e1174fe278086a643c0cb4880fb633e60
VINODvoid <iamlearner.v@gmail.com>
2024-09-17 22:45:00 +0530

    Add asynchronous delay to main function and fix formatting

--- Week-02/05basics.js
+++ Week-02/05basics.js
@@ -1,11 +1,38 @@
 //  Promises is an object that represents the eventual completion ( or Failure ) of an asynchronous operation and its resulting value .
 
 
-
-function main()
-{
-    console.log("Hello");
+// Callback Approach 
+// function main()
+// {
+//     console.log("Hello");
     
-}
+// }
 
-setTimeout(main,3000);
+// setTimeout(main,3000);
+
+
+// Promised Approach 
+
+// function setTimeoutPromisified(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+//   }
+  
+//   function callback() {
+//       console.log("3 seconds have passed");
+//   }
+  
+// setTimeoutPromisified(3000)
+// .then(callback)
+
+
+
+
+// function promiseCallback(resolve)
+// {
+//     setTimeout(resolve,3000);
+// }
+// promiseCallback(function(){
+//     console.log("3 seconds passed.");
+    
+// });
+
