6a959492fe940dc98186871e28b77eb565b711f8
VINODvoid <iamlearner.v@gmail.com>
2024-09-17 19:30:28 +0530

    Add asynchronous delay to main function

---
+++ Week-02/05basics.js
@@ -0,0 +1,11 @@
+//  Promises is an object that represents the eventual completion ( or Failure ) of an asynchronous operation and its resulting value .
+
+
+
+function main()
+{
+    console.log("Hello");
+    
+}
+
+setTimeout(main,3000);
