d9bcbc6078c49e0e0bada750c821485ecba061f5
VINODvoid <iamlearner.v@gmail.com>
2024-09-20 05:44:42 +0530

    feat : reseolve delete function

--- Week-03/script.js
+++ Week-03/script.js
@@ -1,22 +1,20 @@
 let count = 0; 
  function addTodo(){
-    const input = document.querySelector("input");
+    const   input = document.querySelector("input");
     const inputValue = input.value;
     const newElement = document.createElement("div");
     newElement.setAttribute("id",count);
-    count += 1;
-    newElement.innerHTML = "<div>"+inputValue+ "</div><button onclick='deleteTodo("+count+")'>Delete</button>";
-
+    newElement.innerHTML = `<div>${inputValue} </div><button onclick=deleteTodo(${count})>Delete</button>`;
+    
     const body = document.querySelector("body")
     body.appendChild(newElement)
+    count += 1;
+    inputValue=""
  }
 function deleteTodo(index){
     console.log(index);
     
     const todoDiv = document.getElementById(index)
     todoDiv.parentNode.removeChild(todoDiv);
-    // if (todoDiv)
-    // {
-    //     todoDiv.parentNode.removeChild(todoDiv)
-    // }
+    
 }
