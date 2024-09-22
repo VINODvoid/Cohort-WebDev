d5bfed0ab6b27570e188f861218e5c1c185d13b6
VINODvoid <iamlearner.v@gmail.com>
2024-09-22 10:06:37 +0530

    todo : complete the assignment

--- Week-04/assignment/cli/index.js
+++ Week-04/assignment/cli/index.js
@@ -3,6 +3,13 @@ const fs = require("fs");
 
 function main(filePath){
     fs.readFile(filePath,'utf-8',function(err,data){
-        console.log(data)
+        let count = 0;
+        for (let i = 0;i<data.length;i++){
+            if(data[i] === " ")
+                count +=1;
+        }
+        count +=1
+        console.log("There are "+count+" words in file")
     })
 }
+main("./a.txt")
