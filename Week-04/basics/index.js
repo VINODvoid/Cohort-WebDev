128e9dff158568f8c4176661383e006a7a6d0bd6
VINODvoid <iamlearner.v@gmail.com>
2024-09-24 20:24:41 +0530

    Add initial implementation of basics package and index.js

---
+++ Week-04/basics/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+function checkSum(a){
+    let sum  = 0
+    for (let i = 0; i <= a; i++) {
+        sum +=i;
+    }
+    return sum;
+}
+const app = express();
+
+app.get('/',function(req,res){
+    const a = parseInt(req.query.a);
+    const ans = checkSum(a);
+    res.send(ans.toString())
+})
+app.listen(8000)
