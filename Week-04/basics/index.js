a157cdeb7e16428fe4ce4b237b61c6b7c067ce24
VINODvoid <iamlearner.v@gmail.com>
2024-09-27 20:56:59 +0530

    simple in memory kidney check up

--- Week-04/basics/index.js
+++ Week-04/basics/index.js
@@ -1,19 +1,54 @@
 const express = require('express');
 
-function checkSum(a){
-    let sum  = 0
-    for (let i = 0; i <= a; i++) {
-        sum +=i;
-    }
-    return sum;
-}
 const app = express();
+// function checkSum(a){
+//     let sum  = 0
+//     for (let i = 0; i <= a; i++) {
+//         sum +=i;
+//     }
+//     return sum;
+// }
 
-app.get('/',function(req,res){
-    const a = parseInt(req.query.a);
-    const ans = checkSum(a);
-    res.send(ans.toString())
+// app.get('/',function(req,res){
+//     const a = parseInt(req.query.a);
+//     const ans = checkSum(a);
+//     res.send(ans.toString())
+// })
+var users = [
+    {
+        name:"vinod",
+        kidneys :[{
+            healthy:false,
+        },
+        {
+            healthy:true,
+        }
+        ],
+        healthy:true,
+    },
+]
+
+app.get("/",function(req,res){
+    const kidneys = users[0].kidneys
+    const numberOfKidneys = users[0].kidneys.length;
+    let healthyKidneys = 0
+    for (let i = 0; i < numberOfKidneys; i++) {
+        if(kidneys[i].healthy === true){
+            healthyKidneys +=1;
+        }
+    }
+    const unhealthyKidneys = numberOfKidneys-healthyKidneys;
+    res.json(
+        {
+            healthyKidneys,
+            unhealthyKidneys,
+            numberOfKidneys,
+        }
+    )
+
+    
 })
+
 
 
 app.listen(8000,function () {
