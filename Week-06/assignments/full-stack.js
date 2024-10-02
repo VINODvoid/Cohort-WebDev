995c85947cdab82c48b27306ee77412b6bdf719f
VINODvoid <iamlearner.v@gmail.com>
2024-10-02 19:24:22 +0530

    TODO : complete assignment 2

--- Week-06/assignments/full-stack.js
+++ Week-06/assignments/full-stack.js
@@ -1,7 +1,11 @@
-// # Assignment: Creating a TODO app
+ // // # Assignment: Creating a TODO app
 
-// Try to create a TODO application where
+// // Try to create a TODO application where
 
-// 1. User can signup/signin
-// 2. User can create/delete/update TODOs
-// 3. User can see their existing todos and mark them as done
+// // 1. User can signup/signin
+// // 2. User can create/delete/update TODOs
+// // 3. User can see their existing todos and mark them as done\
+
+
+// // Assigment 2  (Link) 
+// https://petal-estimate-4e9.notion.site/Writing-the-frontend-for-it-427523a4a66849af8e63436f25ce941f
