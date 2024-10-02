8c769f94dc48f6da0a6acf133202ee4919d51d54
VINODvoid <iamlearner.v@gmail.com>
2024-10-02 09:18:20 +0530

    feat: Add JWT authentication to sign-in endpoint

--- Week-06/index.js
+++ Week-06/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
-
+const jwt = require('jsonwebtoken');
 
 const app = express();
+const JWT_SECRET = "newuser";
 const port = 3000;
 app.use(express.json());
 
@@ -18,16 +19,18 @@ app.post("/sign-up",function(req,res){
     })
     
 })
-// returns random characters with length 32 
-function generateToken(){
-    const characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","v","y","z","x","w","q","1","2","3","4","5","6","7","8","9","0"];
-    let token = "";
-    for(let i=0;i<32;i++){
-        const randomNumber = Math.floor(Math.random()*characters.length);
-        token += characters[randomNumber];
-    }
-    return token;
-}
+
+// // returns random characters with length 32 
+// function generateToken(){
+//     const characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","v","y","z","x","w","q","1","2","3","4","5","6","7","8","9","0"];
+//     let token = "";
+//     for(let i=0;i<32;i++){
+//         const randomNumber = Math.floor(Math.random()*characters.length);
+//         token += characters[randomNumber];
+//     }
+//     return token;
+// }
+
 app.post("/sign-in",function(req,res){
     const username=req.body.username;
     const password=req.body.password;
@@ -35,8 +38,11 @@ app.post("/sign-in",function(req,res){
     const user = users.find(function(user){
         if(user.username == username && user.password == password)
         {
-            const token = generateToken();
-            user.token = token;
+            // const token = generateToken();
+            const token = jwt.sign({
+                username:username,
+            },JWT_SECRET)
+            // user.token = token;// jwt doesn't need to storage because the token itself gets stored
             res.json({
                 message:token,
             })
@@ -52,7 +58,8 @@ app.post("/sign-in",function(req,res){
 
 app.get("/me",function(req,res){
     const  mytoken = req.headers.token;
-    const foundUser = users.find(user => user.token == mytoken)
+    const decodedInfo= jwt.verify(mytoken,JWT_SECRET)
+    const foundUser = users.find(user => user.username == decodedInfo.username)
     if(foundUser)
     {
         res.json({
