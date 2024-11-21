99c37e1a92fe357d00c93e281d12f3242b40c6c7
VINODvoid <iamlearner.v@gmail.com>
2024-11-21 17:06:50 +0530

    add admin routes

--- Week-08/selling-course-app/index.js
+++ Week-08/selling-course-app/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { userRouter } = require("./routes/user.js");
 const { courseRouter } = require("./routes/course.js");
+const { adminRouter } = require("./routes/admin.js");
 const app = express();
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/course", courseRouter);
-
+app.use("api/v1/admin", adminRouter);
 app.listen(3000);
