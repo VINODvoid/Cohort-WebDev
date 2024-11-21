ab26c30c627073d438634c44a9df0f893e863f44
VINODvoid <iamlearner.v@gmail.com>
2024-11-21 17:06:21 +0530

    add admin routes

---
+++ Week-08/selling-course-app/routes/admin.js
@@ -0,0 +1,31 @@
+const { Router } = require("express");
+
+const adminRouter = Router();
+
+adminRouter.post("/signup", function (req, res) {
+  res.json({
+    message: "signup endpoint",
+  });
+});
+
+adminRouter.post("/signin", function (req, res) {
+  res.json({
+    message: "signin endpoint",
+  });
+});
+
+adminRouter.post("/course-create", function (req, res) {
+  res.json({
+    message: "Course Creation",
+  });
+});
+
+adminRouter.put("/course", function (req, res) {
+  res.json({
+    message: "Course Update",
+  });
+});
+
+module.exports = {
+  adminRouter: adminRouter,
+};
