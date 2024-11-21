247955e606074a2a9fa2179b254e4473ed23f5cf
VINODvoid <iamlearner.v@gmail.com>
2024-11-21 20:43:59 +0530

    feat: connect to DB

---
+++ Week-08/selling-course-app/db/index.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connectDB = async function () {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGO_URI}/course-app`,
+    );
+    console.log("\n MongoDB is connected");
+  } catch (error) {
+    console.log("MongoDB CONNECTION ERROR", error);
+  }
+};
+
+module.exports = connectDB;
--- Week-08/selling-course-app/index.js
+++ Week-08/selling-course-app/index.js
@@ -1,9 +1,12 @@
+require("dotenv").config(); // load environmental variables
 const express = require("express");
 const { userRouter } = require("./routes/user.js");
 const { courseRouter } = require("./routes/course.js");
 const { adminRouter } = require("./routes/admin.js");
+const connectDB = require("./db/index.js");
 const app = express();
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/course", courseRouter);
 app.use("api/v1/admin", adminRouter);
+connectDB();
 app.listen(3000);
