425e8e12a1022057d8f4441229575ec4c17a7d75
VINODvoid <iamlearner.v@gmail.com>
2024-11-23 20:04:33 +0530

    feat: add admin auth scripts

--- Week-08/selling-course-app/routes/admin.js
+++ Week-08/selling-course-app/routes/admin.js
@@ -1,12 +1,36 @@
 const { Router } = require("express");
 const { Admin } = require("../models/admin.model.js");
+const z = require("zod");
+const bcrypt = require("bcryptjs");
 
 const adminRouter = Router();
+const adminSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8, "Password must be at least 8 characters long"),
+  firstName: z.string().optional(),
+  lastName: z.string().optional(),
+});
 
-adminRouter.post("/signup", function (req, res) {
-  res.json({
-    message: "signup endpoint",
-  });
+adminRouter.post("/signup", async function (req, res) {
+  try {
+    const parsedData = adminSchema.parse(req.body);
+    const hashedPassword = await bcrypt.hash(parsedData.password);
+    const admin = new Admin({
+      email: email,
+      password: hashedPassword,
+      firstName: firstName,
+      lastName: lastName,
+    });
+    await admin.save();
+    res.status(201).json({
+      message: "Admin Sign up is successfully !",
+    });
+  } catch (error) {
+    res.status(403).json({
+      message: "sign up failed !!!",
+      error: error,
+    });
+  }
 });
 
 adminRouter.post("/signin", function (req, res) {
