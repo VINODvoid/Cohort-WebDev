adf747ecb2082861f2b1fe935db2e1cd5bd557d2
VINODvoid <iamlearner.v@gmail.com>
2025-01-29 20:06:15 +0530

    chore : pratice

---
+++ Week-09/react-practice-01/src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import {Clock} from 'lucide-react'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{height:"100vh",width:"100vw",backgroundColor:"#dfe6e9",}}>
+      <div style={{display:'flex',justifyContent:"center",}}>
+        <div style={{margin:20}}>
+          <div>
+            <PostComponent
+            name={"Kalki"}
+            description={"lorem ispsum description"}
+            time={"2 min"}
+            subTitle={"100k"}
+            image={"https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg"}
+            />
+            <br />
+          </div>
+        <div>
+            <PostComponent
+            name={"Kalki"}
+            description={"lorem ispsum description"}
+            subTitle={"100k"}
+            image={"https://images.pexels.com/photos/1278566/pexels-photo-1278566.jpeg"}
+            />
+          </div>
+        </div>
+    </div>
+    </div>
+  )
+}
+const style = {borderRadius:10,width:300,borderColor:"gray",backgroundColor:"white",borderWidth:1,padding:20}
+function PostComponent({name, description , time , subTitle,image })
+{
+  return (
+    <div style={style}>
+    <div style={{display:"flex"}}>
+      <img src={image} alt="" style={{
+        width:30,
+        height:30,
+        borderRadius:20,
+      }} />
+      <div style={{marginLeft:10,fontSize:10}}>
+        <b>{name}</b>
+        <div>{subTitle} follower</div>
+        {time!== undefined &&<div style={{display:"flex"}}>
+          <div style={{marginRight:5
+          }}>{time} </div>
+          <Clock style={{width:10,height:10}}/>
+        </div>}
+      </div>
+    </div>
+    <div style={{fontSize:12}}>
+      {description}
+    </div>
+    </div>
+  )
+}
+
+
+
+export default App
