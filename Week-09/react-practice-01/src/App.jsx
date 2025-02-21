a8e450bcbae16fc60a172b241f76c4ea2bfda0bd
VINODvoid <iamlearner.v@gmail.com>
2025-02-21 19:56:30 +0530

    feat: implement PostComponent and add functionality to add posts

--- Week-09/react-practice-01/src/App.jsx
+++ Week-09/react-practice-01/src/App.jsx
@@ -1,8 +1,13 @@
 import { useState } from 'react'
 import {Clock} from 'lucide-react'
 import './App.css'
+import PostComponent from './Post';
 
 function App() {
+  function addPost()
+  {
+    console.log("Post Added");
+  }
   return (
     // <div>
     //   {/* <ToggleMessage/> */}
@@ -11,6 +16,7 @@ function App() {
     //   <Notification/>
     // </div>
     <div style={{height:"100vh",width:"100vw",backgroundColor:"#dfe6e9",}}>
+      <button onClick={addPost}>Add Post</button>
       <div style={{display:'flex',justifyContent:"center",}}>
         <div style={{margin:20}}>
           <div>
@@ -23,46 +29,12 @@ function App() {
             />
             <br />
           </div>
-        <div>
-            <PostComponent
-            name={"Kalki"}
-            description={"lorem ispsum description"}
-            subTitle={"100k"}
-            image={"https://images.pexels.com/photos/1278566/pexels-photo-1278566.jpeg"}
-            />
-          </div>
         </div>
     </div>
     </div>
   )
 }
-const style = {borderRadius:10,width:300,borderColor:"gray",backgroundColor:"white",borderWidth:1,padding:20}
-function PostComponent({name, description , time , subTitle,image })
-{
-  return (
-    <div style={style}>
-    <div style={{display:"flex"}}>
-      <img src={image} alt="" style={{
-        width:30,
-        height:30,
-        borderRadius:20,
-      }} />
-      <div style={{marginLeft:10,fontSize:10}}>
-        <b>{name}</b>
-        <div>{subTitle} follower</div>
-        {time!== undefined &&<div style={{display:"flex"}}>
-          <div style={{marginRight:5
-          }}>{time} </div>
-          <Clock style={{width:10,height:10}}/>
-        </div>}
-      </div>
-    </div>
-    <div style={{fontSize:12}}>
-      {description}
-    </div>
-    </div>
-  )
-}
+
 // Conditional Rendering
 // function ToggleMessage()
 // {
