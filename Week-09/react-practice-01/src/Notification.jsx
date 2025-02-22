39509f0922831f649f00abb0906f000551b69072
VINODvoid <iamlearner.v@gmail.com>
2025-02-22 23:10:03 +0530

    feat: add logging for updated notification count

--- Week-09/react-practice-01/src/Notification.jsx
+++ Week-09/react-practice-01/src/Notification.jsx
@@ -14,6 +14,10 @@ function Notification(){
     useEffect(function(){
         setInterval(increaseCount, 1000);
     },[]);
+    useEffect(()=>{
+        console.log("Count is updated"+count);
+        
+    },[count])
 
     return (
         <div>
@@ -23,4 +27,4 @@ function Notification(){
     )
 }
 
-export default Notification;
+export default Notification; 
