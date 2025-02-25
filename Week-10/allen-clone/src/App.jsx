1101948692f191a2a45c42565e0a7f539512d007
VINODvoid <iamlearner.v@gmail.com>
2025-02-25 21:22:57 +0530

    feat: add routing with React Router and implement AppLayout for improved navigation structure

--- Week-10/allen-clone/src/App.jsx
+++ Week-10/allen-clone/src/App.jsx
@@ -1,7 +1,18 @@
+import {Routes,Route} from 'react-router-dom'
+import AppLayout from './AppLayout'
 function App() {
   return (
    <div>
-    
+   <Routes>
+      <Route path='/' element={<AppLayout/>} >
+        <Route path='/courses' />
+        <Route path='/test-series' />
+        <Route path='/scholarships' />
+        <Route path='/results' />
+        <Route path='/study-materials' />
+        <Route path='/about-us' />
+      </Route>
+   </Routes>
    </div>
   )
 }
---
+++ Week-10/allen-clone/src/AppLayout.jsx
@@ -0,0 +1,11 @@
+
+
+export const AppLayout = () => {
+  return (
+    <div style={{height:'100vh'}}>
+        {/* Header*/}
+        {/* Main Content */}
+        {/* Footer */}
+    </div>
+  )
+}
