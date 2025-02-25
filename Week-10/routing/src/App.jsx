2a3b837b5a20f0fe1b9fd57b0be06ec3f665c98a
VINODvoid <iamlearner.v@gmail.com>
2025-02-25 20:35:53 +0530

    feat: implement routing with React Router and create basic route components

--- Week-10/routing/src/App.jsx
+++ Week-10/routing/src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {BrowserRouter , Routes , Route, Link } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <BrowserRouter>
+      <Link to={"/"}>Home</Link>
+        <Link to={"/route-1"}>Route 1 </Link>
+        <Link to={"/route-2"}>Route 2 </Link>
+        <Link to={"/route-3"}>Route 3 </Link>
+      <Routes> 
+        <Route path='/' element={<Home/>} />
+        <Route path='/route-1' element={<Component1/>} />
+        <Route path='/route-2' element={<Component2/>} />
+        <Route path='/route-3' element={<Component3/>} />
+        <Route path='/*' element={<PageNotFound/>} />
+      </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
+function Home()
+{
+  return <div>
+    Home 
+  </div>
+}
+
+function Component1()
+{
+  return <div>
+    Route to 1 
+  </div>
+}
+function Component2()
+{
+  return <div>
+    Route to 2 
+  </div>
+}
+function Component3()
+{
+  return <div>
+    Route to 3
+  </div>
+}
+function PageNotFound()
+{
+  return <div>
+    Sorry you have reached the pinnacle of the MISERY.
+  </div>
+}
 export default App
