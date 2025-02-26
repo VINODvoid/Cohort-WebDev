2ab7fc185d7f6a218efea305d38124adeb1ab7ae
VINODvoid <iamlearner.v@gmail.com>
2025-02-26 18:48:54 +0530

    feat: add Header component and update AppLayout for improved structure; fix routing for courses

--- Week-10/allen-clone/src/App.jsx
+++ Week-10/allen-clone/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
    <div>
    <Routes>
       <Route path='/' element={<AppLayout/>} >
-        <Route path='/courses' />
+        <Route path='/courses' element={<></>}/>
         <Route path='/test-series' />
         <Route path='/scholarships' />
         <Route path='/results' />
--- Week-10/allen-clone/src/AppLayout.jsx
+++ Week-10/allen-clone/src/AppLayout.jsx
@@ -1,11 +1,15 @@
+import Header from "./Header"
 
 
-export const AppLayout = () => {
+const AppLayout = () => {
   return (
     <div style={{height:'100vh'}}>
         {/* Header*/}
+        <Header/>
         {/* Main Content */}
         {/* Footer */}
     </div>
-  )
-}
+  );
+};
+
+export default AppLayout;
