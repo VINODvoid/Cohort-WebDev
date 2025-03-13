6b3b174cf9d6c1ae06dd7d472c962bba72550a07
VINODvoid <iamlearner.v@gmail.com>
2025-03-13 07:15:29 +0530

    feat: implement custom useCounter hook in App component

--- Week-11/customhooks/src/App.jsx
+++ Week-11/customhooks/src/App.jsx
@@ -1,13 +1,29 @@
+import { useState } from 'react'
 import './App.css'
 
-function App() {
+// custom hooks
+function useCounter()
+{
+  const [count,setCount ] = useState(0);
+  function increaseCount()
+  {
+    setCount(c=>c+1)
+  }
+  return {
+    count:count,
+    increaseCount:increaseCount
+  }
+}
 
+function App() {
+  const {count,increaseCount} = useCounter();
   return (
     <div>
-      hi there
-    
+      <button onClick={increaseCount}>Add</button>
+      {count}
     </div>
   )
 }
 
 export default App
+ 
