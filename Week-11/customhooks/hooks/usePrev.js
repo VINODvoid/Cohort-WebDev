a3ec5f23bf7873f024df3b157b68917c8015c8f6
VINODvoid <iamlearner.v@gmail.com>
2025-04-01 20:52:03 +0530

    feat: add console logs to usePrev hook for debugging value updates

--- Week-11/customhooks/hooks/usePrev.js
+++ Week-11/customhooks/hooks/usePrev.js
@@ -2,8 +2,14 @@ import { useEffect, useRef } from "react"
 
 export const usePrev = (value) => {
     const ref = useRef();
+    console.log("rerendered with new valuer"+value);
+    
     useEffect(()=>{
+        console.log("updated ref to new value "+value);
+        
         ref.current = value;
+
     },[value])
+    console.log("returned"+ref.current);
     return ref.current;
 }
