4492228d5b9589bf548e0f3a8e2398ed1d8afe8d
VINODvoid <iamlearner.v@gmail.com>
2025-04-02 17:11:08 +0530

    feat : refractor useDeBounce hook for value handling

--- Week-11/customhooks/hooks/useDebounce.js
+++ Week-11/customhooks/hooks/useDebounce.js
@@ -1,11 +1,28 @@
-import { useRef } from "react"
+import { useEffect,useState } from "react"
 
-export const useDeBounce = (originalfn) =>{
-    const  currentClock = useRef();
-    const fn = () =>{
-        clearTimeout(currentClock.current);
-        currentClock.current = setTimeout(originalfn,200);
-    }
+// export const useDeBounce = (originalfn) =>{
+//     const  currentClock = useRef();
+//     const fn = () =>{
+//         clearTimeout(currentClock.current);
+//         currentClock.current = setTimeout(originalfn,200);
+//     }
 
-    return fn;
-}
+//     return fn;
+// }
+
+
+export const useDebounce = (value, delay) => {
+    const [debouncedValue, setDebouncedValue] = useState(value);
+
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            setDebouncedValue(value);
+        }, delay);
+
+        return () => {
+            clearTimeout(handler);
+        };
+    }, [value, delay]);
+
+    return debouncedValue;
+};
