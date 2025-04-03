1e914ba5ba40cf6e427faf5ac3dd89ec67304135
VINODvoid <iamlearner.v@gmail.com>
2025-04-03 22:57:22 +0530

    feat: add loadingAtom for managing loading state in Recoil

--- Week-11/state-management/src/store/atoms/Counter.js
+++ Week-11/state-management/src/store/atoms/Counter.js
@@ -3,4 +3,8 @@ import { atom } from "recoil";
 export const counterAtom = atom({
     key:"counter",
     default:0
+});
+export const loadingAtom = atom({
+    key:"loading",
+    default:true,
 })
