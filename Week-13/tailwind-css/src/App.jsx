b8286da8ffe48bb532dbf69a17f0e813febb08c0
VINODvoid <iamlearner.v@gmail.com>
2025-04-09 06:28:24 +0530

    feat: update App component layout with Flexbox and Grid structures

--- Week-13/tailwind-css/src/App.jsx
+++ Week-13/tailwind-css/src/App.jsx
@@ -4,7 +4,18 @@ function App() {
 
   return (
     <>
-    <div className='text-4xl'>Hi</div>
+    <div className='text-center text-3xl'>Flex Box</div>
+    <div className='text-4xl bg-blue-300 rounded-3xl  flex justify-between '>
+    <div className='bg-green-400 rounded-3xl'>Child 1</div>
+    <div className='bg-yellow-400 rounded-3xl'>Child 2</div>
+    <div className='bg-red-500 rounded-3xl'>Child 3</div>
+    </div>
+    <div className='text-center text-3xl '>Grids</div>
+    <div className='grid grid-cols-12 rounded-3xl'>
+      <div className='col-span-4 bg-green-300 '>Child 1</div>
+      <div className='col-span-4 bg-yellow-300'>Child 2</div>
+      <div className='col-span-4 bg-red-400'>Child 3</div>
+    </div>
     </>
   )
 }
