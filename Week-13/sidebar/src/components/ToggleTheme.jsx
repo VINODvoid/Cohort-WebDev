6f118647a2e8028b74632f174729e28a391eb035
VINODvoid <iamlearner.v@gmail.com>
2025-04-18 23:49:48 +0530

    feat: refactor ToggleTheme component to encapsulate theme switching logic

--- Week-13/sidebar/src/components/ToggleTheme.jsx
+++ Week-13/sidebar/src/components/ToggleTheme.jsx
@@ -3,15 +3,26 @@ import React, {  useState} from 'react'
 import { cn } from '../lib/utils'
 
 const ToggleTheme = () => {
-    const [toggleTheme , setToggleTheme] = useState(false);
+    
   return (
     < >
-    <div className={cn(toggleTheme?"dark:bg-blue-400 text-green":"bg-white text-black")} onClick={()=>{
-        setToggleTheme(!toggleTheme)
-    }}>{toggleTheme?<Moon/>:<Sun/>}
+    <div className={cn()}>
+   <SwitchTheme/>
     </div>
     </>
   )
 }
+
+function SwitchTheme()
+{
+    const [toggleTheme , setToggleTheme] = useState(false);
+return (
+    <div onClick={()=>{
+        setToggleTheme(!toggleTheme)
+    }}>{toggleTheme?<Moon/>:<Sun/>}
+    </div>
+)
+}
+
 
 export default ToggleTheme
