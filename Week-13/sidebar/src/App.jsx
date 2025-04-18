709559b4b60054a54797c5df06eaf223ed7e348e
VINODvoid <iamlearner.v@gmail.com>
2025-04-18 23:15:22 +0530

    feat: add utility functions and integrate new dependencies for theme toggling

--- Week-13/sidebar/src/App.jsx
+++ Week-13/sidebar/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Siderbar from './components/Siderbar'
+import ToggleTheme from './components/ToggleTheme'
 
 function App() {
 
   return (
     <>
-    <Siderbar/>
+    {/* <Siderbar/> */}
+    <ToggleTheme/>
     </>
   )
 }
--- Week-13/sidebar/src/components/ToggleTheme.jsx
+++ Week-13/sidebar/src/components/ToggleTheme.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import { Moon, Sun } from 'lucide-react';
+import React, {  useState} from 'react'
+import { cn } from '../lib/utils'
 
 const ToggleTheme = () => {
+    const [toggleTheme , setToggleTheme] = useState(false);
   return (
-    <div className=''>
-
+    < >
+    <div className={cn(toggleTheme?"dark:bg-blue-400 text-green":"bg-white text-black")} onClick={()=>{
+        setToggleTheme(!toggleTheme)
+    }}>{toggleTheme?<Moon/>:<Sun/>}
     </div>
+    </>
   )
 }
 
