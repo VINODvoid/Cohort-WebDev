908110d9673393920b2c12d805109a16d1ea8b40
VINODvoid <iamlearner.v@gmail.com>
2025-04-19 08:24:12 +0530

    feat: update ToggleTheme component and package dependencies for improved theme handling

--- Week-13/sidebar/src/components/ToggleTheme.jsx
+++ Week-13/sidebar/src/components/ToggleTheme.jsx
@@ -1,28 +1,35 @@
-import { Moon, Sun } from 'lucide-react';
-import React, {  useState} from 'react'
-import { cn } from '../lib/utils'
+import { Moon, Sun } from "lucide-react";
+import React, { useState } from "react";
+import { cn } from "../lib/utils";
 
 const ToggleTheme = () => {
-    
   return (
-    < >
-    <div className={cn()}>
-   <SwitchTheme/>
-    </div>
+    <>
+      <div className={cn()}>
+        <SwitchTheme />
+      </div>
     </>
-  )
-}
+  );
+};
 
-function SwitchTheme()
-{
-    const [toggleTheme , setToggleTheme] = useState(false);
-return (
-    <div onClick={()=>{
-        setToggleTheme(!toggleTheme)
-    }}>{toggleTheme?<Moon/>:<Sun/>}
+function SwitchTheme() {
+  const [toggleTheme, setToggleTheme] = useState(false);
+  return (
+    <div
+      onClick={() => {
+        setToggleTheme(!toggleTheme);
+      }}
+    >
+      {toggleTheme ? (
+        <Moon className="text-white" />
+      ) : (
+        <Sun className="text-yellow-500" />
+      )}
+      <div className="bg-white text-black dark:bg-black dark:text-white">
+        Your content here
+      </div>
     </div>
-)
+  );
 }
 
-
-export default ToggleTheme
+export default ToggleTheme;
