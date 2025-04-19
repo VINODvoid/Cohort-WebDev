20bb39747acc76046bb2bfb10a5ab22079536ff5
VINODvoid <iamlearner.v@gmail.com>
2025-04-19 20:18:48 +0530

    feat: restructure App component layout and update MainComponent rendering

--- Week-13/sidebar/src/App.jsx
+++ Week-13/sidebar/src/App.jsx
@@ -5,11 +5,36 @@ import ToggleTheme from './components/ToggleTheme'
 function App() {
 
   return (
-    <>
+    <div className='flex'>
     {/* <Siderbar/> */}
-    <ToggleTheme/>
-    </>
+    {/* <ToggleTheme/> */}
+    {/* <Sidebar/> */}
+    <MainComponent/>
+
+    </div>
   )
 }
+
+function MainComponent()
+{
+  return(
+      // <div className='grid grid-cols-12'>
+      //   <div className="col-span-6 bg-amber-400"></div>
+      //   <div className="col-span-4 bg-green-400"></div>
+      //   <div className="col-span-2 bg-red-400"></div>
+      // </div>
+      <div class="grid grid-flow-col grid-rows-3 gap-4">
+  <div class="row-span-3 ">01</div>
+  <div class="col-span-2 ...">02</div>
+  <div class="col-span-2 row-span-2 ...">03</div>
+</div>
+  )
+}
+// function Sidebar()
+// {
+//   return (
+//     <div>HI</div>
+//   )
+// }
 
 export default App
