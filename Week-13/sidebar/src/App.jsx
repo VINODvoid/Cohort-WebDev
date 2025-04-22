c2183bcfb15be744db518f3e11044b7bdf832a23
VINODvoid <iamlearner.v@gmail.com>
2025-04-22 10:50:07 +0530

    fix: remove unnecessary whitespace in MainComponent function

--- Week-13/sidebar/src/App.jsx
+++ Week-13/sidebar/src/App.jsx
@@ -23,6 +23,7 @@ function MainComponent()
       //   <div className="col-span-4 bg-green-400"></div>
       //   <div className="col-span-2 bg-red-400"></div>
       // </div>
+      
       <div class="grid grid-flow-col grid-rows-3 gap-4">
   <div class="row-span-3 ">01</div>
   <div class="col-span-2 ...">02</div>
