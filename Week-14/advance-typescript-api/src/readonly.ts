a84e297913750795f0dce6efed80873b11de4954
VINODvoid <iamlearner.v@gmail.com>
2025-05-01 23:23:25 +0530

    feat: add readonly.ts for demonstrating readonly properties in TypeScript; update tsconfig to include it

---
+++ Week-14/advance-typescript-api/src/readonly.ts
@@ -0,0 +1,25 @@
+const a:number[] = [1,2,3,4];
+
+a[0] = 100;
+console.log(a);
+
+
+// even though we have assingned const , the data is changed because the reference of the variable is not changed , the data internally can be changed 
+
+
+
+
+type Angels = {
+    readonly name : string,
+    realm:string,
+    age:number
+}
+
+let black:Angels =  {
+    name: "angels",
+    age: 306,
+    realm: "hell",
+}
+
+// so by readonly property we can make one of the pairs constants after initialisation
+console.log();
