cbf886633f60f89990e4aa7a0332553f5b1f28e1
VINODvoid <iamlearner.v@gmail.com>
2025-05-02 09:29:46 +0530

    feat: add map.ts for storing and retrieving Mens data; update tsconfig to include map.ts

---
+++ Week-14/advance-typescript-api/src/map.ts
@@ -0,0 +1,8 @@
+const Mens= new Map();
+
+Mens.set(1,{name:"man1",age:"23"})
+Mens.set(2,{name:"man2",age:"67"})
+Mens.set(3,{name:"man3",age:"12"})
+Mens.set(4,{name:"man4",age:"39"})
+
+console.log(Mens.get(1));
