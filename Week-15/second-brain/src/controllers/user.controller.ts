d68278e294cfa363f919850e7243e3428cc195e2
VINODvoid <iamlearner.v@gmail.com>
2025-05-03 19:50:28 +0530

    refactor: clean up user registration controller and routes for improved readability

--- Week-15/second-brain/src/controllers/user.controller.ts
+++ Week-15/second-brain/src/controllers/user.controller.ts
@@ -1,43 +1,30 @@
+import { NextFunction, Request, Response } from 'express';
 import { z } from 'zod';
 import { User } from '../models/user.model';
-import { Request, Response } from 'express';
 
-
-
-// zod validation 
 const registerUserSchema = z.object({
     username: z.string().min(3).max(10),
     password: z.string().min(8).max(20)
-})
+});
 
-
-export const registerUser = async (req: Request, res: Response) => {
+export const registerUser = async (req: Request, res: Response ,next:NextFunction)=> {
     try {
-
-        // here it validate the req.body with schema
-        const validateData = registerUserSchema.parse(req.body);
-        const { username, password } = validateData;
-        // 
+        const validatedData = registerUserSchema.parse(req.body);
+        const { username, password } = validatedData;
 
         const existingUser = await User.findOne({ username });
         if (existingUser) {
             return res.status(403).json({ message: 'User already exists with this username' });
         }
+
         const user = new User({ username, password });
         await user.save();
-        res.status(200).json({
-            message: "User Registered Successfully"
-        })
+        return res.status(200).json({ message: "User Registered Successfully" });
 
     } catch (error) {
         if (error instanceof z.ZodError) {
-            res.status(411).json({
-                errors: error.errors[0].message
-            })
+            return res.status(411).json({ errors: error.errors[0].message });
         }
-        res.status(500).json({
-            message: "Internal Server Error"
-        })
+        next(error);
     }
-
-}
+};
--- Week-15/second-brain/src/routes/user.routes.ts
+++ Week-15/second-brain/src/routes/user.routes.ts
@@ -1,17 +1,14 @@
-import { Router } from "express"
-import { registerUser } from "../controllers/user.controller";
+import { Router } from 'express';
+import { registerUser } from '../controllers/user.controller';
 
 export const UserRouter = Router();
 
-// below code we can use url like localhost:3000/api/v1/signup
-UserRouter.post("/signup", registerUser); // Cleaner and preferred
 
-UserRouter.post("/signin",(req,res)=>{
-    console.log("singin");
-    res.send("singin")
+UserRouter.post("/signup",function(req,res,next){
+    try {
+        registerUser(req,res,next)
+    } catch (error) {      
+    }
 })
-
-
-
 
 
