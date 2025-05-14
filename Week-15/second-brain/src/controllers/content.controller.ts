be2dec4444326dfb3cd72e576e4ec3d685e549c0
VINODvoid <iamlearner.v@gmail.com>
2025-05-14 20:33:19 +0530

    feat: enhance content controller with async handling and improve display logic

--- Week-15/second-brain/src/controllers/content.controller.ts
+++ Week-15/second-brain/src/controllers/content.controller.ts
@@ -1,7 +1,34 @@
 import { Request, Response } from "express";
+import { Content } from "../models/content.model";
 
-export const addContent = (req:Request,res:Response)=>{
+export const addContent = async(req:Request,res:Response)=>{
+    try{
     const {link,type,title} = req.body;
-    
+    const content =  new Content({
+        link,
+        type,
+        title
+    });
+    await content.save();
+    res.json({ message: "Content added" });
+    }
+    catch(e)
+    {
+        res.status(403).json({
+            message:"Content not added"
+        })
+    }
 }
+export const displayContent = (req:Request,res:Response)=>{
+    try {
+        //@ts-ignore
+        const userId= req.userId;
 
+        const content = Content.find({userId}).populate("userId","username");
+        res.json(content);
+    } catch (error) {
+        res.status(403).json({
+            message:"Not able to display contents"
+        })
+    }
+}
--- Week-15/second-brain/src/routes/content.routes.ts
+++ Week-15/second-brain/src/routes/content.routes.ts
@@ -5,3 +5,5 @@ import { addContent } from "../controllers/content.controller";
 export  const ContentRouter = Router();
 
 ContentRouter.post("/content", userMiddleware,addContent );
+
+ContentRouter.get("/content",userMiddleware,)
