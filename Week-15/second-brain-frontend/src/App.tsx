6b745566a106631affcb86912b599fc022023b9c
VINODvoid <iamlearner.v@gmail.com>
2025-05-15 08:59:03 +0530

    feat: add lucide-react icons and enhance Button component with new props

--- Week-15/second-brain-frontend/src/App.tsx
+++ Week-15/second-brain-frontend/src/App.tsx
@@ -1,13 +1,17 @@
 import './App.css'
 import Button from './components/ui/Button'
-
+import {PlusIcon} from "lucide-react"
+import ShareIcon from './icons/ShareIcon'
 function App() {
 
   return (
     <>
-    <Button/>
+    <Button onClick={()=>{}}  text='Add Content' type='primary' size='sm' startIcon={<PlusIcon/>} />
+    <Button onClick={()=>{}}  text='Share Brain' type='secondary' size='md' startIcon={<ShareIcon/>}/>
+    <Button onClick={()=>{}}  text='Large' type='primary' size='lg' />
     </>
   )
 }
+
 
 export default App
--- Week-15/second-brain-frontend/src/components/ui/Button.tsx
+++ Week-15/second-brain-frontend/src/components/ui/Button.tsx
@@ -1,17 +1,33 @@
+import type { ReactElement } from "react"
 
 
-export interface ButtonProps {
+interface ButtonProps {
     type:"primary" | "secondary",
     size :"sm"| "md" | "lg",
     text:string,
-    startIcon : any,
+    startIcon ?: ReactElement,
     endIcon?:any
     onClick:()=>void
 }
 
+const VariantStyles = {
+    "primary" : "bg-purple-600 text-white",
+    "secondary" :"bg-purple-200 text-purple-500",
+}
+const sizeStyles = {
+    "sm" : "px-2 py-1",
+    "md" :"px-4 py-2",
+    "lg":"py-8 px-4"
+}
+const defaultStyles = "rounded-md  m-2 flex"
+
+
 const Button = (props:ButtonProps) => {
   return (
-    <button className="text-2xl shadow-2xl">Button</button>
+    <button className={`${defaultStyles} ${VariantStyles[props.type]} ${sizeStyles[props.size]}`}>
+        {props.startIcon ? <div className="pr-2">{props.startIcon}</div>:null}
+        {props.text}
+    </button>
   )
 }
 
