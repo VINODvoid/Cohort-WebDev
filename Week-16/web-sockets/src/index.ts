3d08c351eb9c601b0691175212ce454f2b7d4380
VINODvoid <iamlearner.v@gmail.com>
2025-05-18 10:44:43 +0530

    feat: implement WebSocket server for real-time price updates

--- Week-16/web-sockets/src/index.ts
+++ Week-16/web-sockets/src/index.ts
@@ -1,1 +1,16 @@
-console.log("web sockets");
+import {WebSocketServer} from "ws"
+
+const wss = new WebSocketServer({port:3000});
+
+
+wss.on("connection",(socket)=>{
+    console.log("User connnected");
+
+    setInterval(() => {
+        socket.send("Current price of solana is"+ (Math.random())*100);
+    }, 5000);
+    socket.on("message",(e)=>{
+        console.log(e.toString())
+    })
+})
+
