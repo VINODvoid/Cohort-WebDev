e1ba90c7fdc081bec8a2cd1127b14abdeaaef293
VINODvoid <iamlearner.v@gmail.com>
2025-05-18 13:45:49 +0530

    feat : understanding web-sockets

--- Week-16/web-sockets-fe/src/App.tsx
+++ Week-16/web-sockets-fe/src/App.tsx
@@ -3,29 +3,21 @@ import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [socket ,setSocket] = useState<WebSocket | undefined>();
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  function sendMessage()
-  {
-    if (!socket) return;
-    const message = inputRef.current.value;
-    socket.send(message);
-  }
+  const inputValue = useRef<HTMLInputElement>(null);
   useEffect(()=>{
     const ws = new WebSocket("ws://localhost:3000");
-    setSocket(ws);
-    ws.onmessage = (event)=>{
-      console.log(event.data);
-    }
-
-  },
-[])
+    ws.onmessage=((e)=>{
+      console.log(e.data);
+    })
+  },[]);
+  function sendMessage()
+  {
+  }
   return (
     <>
     <div>
-      <input type="text" placeholder='Enter the message ...' ref={inputRef}/>
-      <button onClick={sendMessage}>Send</button>
+      <input type="text" placeholder='Enter the Message' ref={inputValue} />    
+      <button onClick={sendMessage} >Send</button>
     </div>
     </>
   )
