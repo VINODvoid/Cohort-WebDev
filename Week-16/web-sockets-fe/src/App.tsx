6e3a50e75cbc55eed692c9ccec00849464f330ca
VINODvoid <iamlearner.v@gmail.com>
2025-05-18 14:14:09 +0530

    feat : added onclick function

--- Week-16/web-sockets-fe/src/App.tsx
+++ Week-16/web-sockets-fe/src/App.tsx
@@ -3,15 +3,22 @@ import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const [socket , setSocket] = useState<WebSocket| undefined>();
   const inputValue = useRef<HTMLInputElement>(null);
+  
   useEffect(()=>{
     const ws = new WebSocket("ws://localhost:3000");
     ws.onmessage=((e)=>{
       console.log(e.data);
-    })
+    });
+    setSocket(ws);
+
   },[]);
   function sendMessage()
   {
+    const message:string | undefined = inputValue.current?.value;
+    socket?.send(typeof(message)=== "string" ? message :"");
   }
   return (
     <>
