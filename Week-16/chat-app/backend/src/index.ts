a08ab9aab7802af963609a7e6b6693516ee90e9c
VINODvoid <iamlearner.v@gmail.com>
2025-05-19 21:14:29 +0530

    feat : conditional messaging

--- Week-16/chat-app/backend/src/index.ts
+++ Week-16/chat-app/backend/src/index.ts
@@ -1,11 +1,12 @@
 import { WebSocketServer } from "ws";
 
-const wss = new WebSocketServer({
-    port:3000
-});
+const wss = new WebSocketServer({port:3000});
 wss.on("connection",(socket)=>{
-    console.log("Connected to web socket");
-    
-    socket.send("hello")
-    
-})
+    socket.onmessage = (event)=>{
+        if(event.data ==="hi")
+        {
+            socket.send("hello");
+        }
+    }
+
+});
