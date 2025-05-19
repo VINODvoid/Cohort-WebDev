91b1766b97e4a5956e197329cd3232823d579a6c
VINODvoid <iamlearner.v@gmail.com>
2025-05-19 07:37:20 +0530

    feat: create ws server

--- Week-16/chat-app/backend/src/index.ts
+++ Week-16/chat-app/backend/src/index.ts
@@ -1,1 +1,11 @@
-console.log("hi");
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({
+    port:3000
+})
+wss.on("connection",(socket)=>{
+    console.log("Connected to web socket");
+    
+    socket.send("hello")
+    
+})
