ec781a856b413430dff63aefd9bc1fd1a0b4fadd
VINODvoid <iamlearner.v@gmail.com>
2025-05-23 08:21:33 +0530

    fix : SQL injection

--- Week-17/postgres/src/index.ts
+++ Week-17/postgres/src/index.ts
@@ -1,4 +1,9 @@
 import { Client } from "pg";
+import express, { Request, Response } from "express";
+
+const app = express();
+app.use(express.json());
+
 
 const client = new Client({
     user:"postgres",
@@ -8,14 +13,26 @@ const client = new Client({
     port:5432,
 })
 
+client.connect();
+console.log("Db connected");
 
-async function connectDb()
-{
-    await client.connect();
-    console.log("Db connected");
-    const response = await client.query("SELECT * FROM users");
-    console.log(response.rows[0]);
-    
-}
+app.post("/signup",async function(req:Request,res:Response){
+    try {
+        const {username, email , password } = req.body;
+        // const response = await client.query("INSERT INTO users (username, email, password) VALUES ($1, $2, $3)",[username, email, password]);
+        const response = await client.query("select * from users;")
+        console.log(response);
+        res.status(200).json({
+            message:"User registerd"
+        })
+        return ;
+    } catch (error) {
+        res.status(501).json({
+            message:"error"+error
+        });
+        return;
+        
+    }
 
-connectDb()
+})
+app.listen(3000);
