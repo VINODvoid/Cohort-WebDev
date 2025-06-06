fcd063b2aa1a2f32a41e22e18170ae56357f502c
VINODvoid <iamlearner.v@gmail.com>
2025-06-06 12:48:35 +0530

    feat : format the file

--- Week-19/next-learn/app/user/page.tsx
+++ Week-19/next-learn/app/user/page.tsx
@@ -1,9 +1,9 @@
-
 import axios from "axios";
-export async function  User()
-{
-    const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details");
-    const data =  response.data;
-    return data;
-
+export async function User() {
+  const response = await axios.get(
+    "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details",
+  );
+  const data = response.data;
+  return data;
 }
+
