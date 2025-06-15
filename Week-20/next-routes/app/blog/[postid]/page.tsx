243831e41d9fdd9a8a4a67c1082368e7c25b1ac1
VINODvoid <iamlearner.v@gmail.com>
2025-06-15 21:23:27 +0530

    feat: add axios dependency and create blog post page component

---
+++ Week-20/next-routes/app/blog/[postid]/page.tsx
@@ -0,0 +1,18 @@
+import axios from 'axios'
+import React from 'react'
+
+const page = async({params}:{params:{
+    postid:number
+}}) => {
+    const response =  await axios.get(`https://jsonplaceholder.typicode.com/posts/${params.postid}`)
+    const todo = response.data;
+  return (
+    <>
+    <div>Blog Page {params.postid}</div>
+    <div>{todo.title}</div>
+    <div>{todo.body}</div>
+    </>
+  )
+}
+
+export default page
