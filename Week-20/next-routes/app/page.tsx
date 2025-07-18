bbb83727344261c8c431b70df889b9984a2e213b
VINODvoid <iamlearner.v@gmail.com>
2025-07-18 13:40:44 +0530

    feat : update

--- Week-20/next-routes/app/page.tsx
+++ Week-20/next-routes/app/page.tsx
@@ -1,9 +1,19 @@
+"use client"
+import axios from "axios";
+import { redirect } from "next/dist/server/api-utils";
+import { useState } from "react";
 
 
 export default function Home() {
+  const [blogId,setBlogId]=useState(0);
+  function blogdetails()
+  {
+    const data = axios.post("http://localhost:3000/blog/1")
+    console.log(data);
+  }
   return (
     <div>
-      
+    <button onClick={blogdetails}>Click</button>    
     </div>
   );
 }
