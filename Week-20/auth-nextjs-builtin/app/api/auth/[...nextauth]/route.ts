995f0f531610ea808cbc9fdb369a4a344442e1da
kalki <kalki.the.dev@gmail.com>
2025-08-09 16:09:59 +0530

    fix: correct NextAuth configuration and update Home component to use getServerSession

--- Week-20/auth-nextjs-builtin/app/api/auth/[...nextauth]/route.ts
+++ Week-20/auth-nextjs-builtin/app/api/auth/[...nextauth]/route.ts
@@ -43,7 +43,8 @@ const handler = NextAuth({
 
   })
 
-]
-});
+],
+secret:process.env.NEXTAUTH_SECRET,
+},);
 
 export {handler as GET , handler as POST}
--- Week-20/auth-nextjs-builtin/app/page.tsx
+++ Week-20/auth-nextjs-builtin/app/page.tsx
@@ -1,21 +1,25 @@
-"use client"
-import { SessionProvider, signIn, signOut, useSession } from "next-auth/react";
+import { getSession, SessionProvider, signIn, signOut, useSession ,} from "next-auth/react";
+import {getServerSession} from "next-auth"
 
+export default async function Home() {
+  const session = await getServerSession();
 
-export default function Home() {
   return (
-    <SessionProvider>
-      <HomeContent/>
-    </SessionProvider>
-  );
-}
-
-function HomeContent()
-{
-  const session = useSession();
-  return (
-    <div className="flex items-center justify-center py-16 min-h-screen">
-      {session.status === "authenticated"?<button onClick={()=>signOut()}>log out</button>:<button onClick={()=>signIn()}>Sign in</button>}
+    <div>
+      {JSON.stringify(session)}
     </div>
+    // <SessionProvider>
+    //   <HomeContent/>
+    // </SessionProvider>
   );
 }
+
+// function HomeContent()
+// {
+//   const session = useSession();
+//   return (
+//     <div className="flex items-center justify-center py-16 min-h-screen">
+//       {session.status === "authenticated"?<button onClick={()=>signOut()}>log out</button>:<button onClick={()=>signIn()}>Sign in</button>}
+//     </div>
+//   );
+// }
